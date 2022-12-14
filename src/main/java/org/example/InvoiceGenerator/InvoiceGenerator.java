dcfaaf94d01d49839756204ae03d61142e0ea0cb
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-12-14 23:56:50 +0530

    Added Enhanced Invoice

--- src/main/java/org/example/InvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/org/example/InvoiceGenerator/InvoiceGenerator.java
@@ -16,5 +16,22 @@ public class InvoiceGenerator {
             totalFare+=this.calculateFare(ride.distance,ride.time);
         return new InvoiceSummary(rides.length,totalFare);
     }
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides)
+            totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+        return totalFare;
+    }
+
+    public int numberOfRides(Ride[] rides) {
+        return rides.length;
+    }
+
+    public double calculateAverageFarePerRide(Ride[] rides) {
+        double totalFare = this.calculateTotalFare(rides);
+        double numberOfRides = rides.length;
+        double averageFare = totalFare/numberOfRides;
+        return averageFare;
+    }
 
 }
